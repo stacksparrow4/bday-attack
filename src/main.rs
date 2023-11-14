a2eb72d5c70a3d7112da9ba96a4852e4d3ec5d34
Daniel Cooper <stacksparrow4@gmail.com>
2023-11-14 11:40:39 +1100

    Basic set version, crashing however

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use constants::NumSpacesType;
 use hash::HashLastDigits;
-use rustc_hash::FxHashMap;
+use rustc_hash::FxHashSet;
 use std::hash::BuildHasherDefault;
 use std::sync::Arc;
 use std::time::Instant;
@@ -11,12 +11,14 @@ mod hash_gen;
 mod progress_updater;
 mod sha;
 
-use crate::constants::{HASH_SEARCH_WORKER_THREADS, NUM_HASHES};
+use crate::constants::{DESIRED_HEX_MATCHES, HASH_SEARCH_WORKER_THREADS, NUM_HASHES};
 use crate::hash::HashLastDigitsPair;
 use crate::hash_gen::{get_hashes_in_threads, get_reversed_hashes};
 use crate::progress_updater::Progress;
 
-fn gen_table() -> FxHashMap<HashLastDigits, NumSpacesType> {
+type TableType = FxHashSet<HashLastDigits>;
+
+fn gen_table() -> TableType {
     println!("Generating hash table...");
     let now = Instant::now();
 
@@ -24,42 +26,63 @@ fn gen_table() -> FxHashMap<HashLastDigits, NumSpacesType> {
 
     let hashes_fake = get_reversed_hashes(include_str!("confession_fake.txt"), NUM_HASHES);
 
-    let mut hash_map =
-        FxHashMap::with_capacity_and_hasher(NUM_HASHES as usize, BuildHasherDefault::default());
+    let mut table =
+        FxHashSet::with_capacity_and_hasher(NUM_HASHES as usize, BuildHasherDefault::default());
 
     while let Ok(fake_hashes) = hashes_fake.recv() {
         for fake_hash in fake_hashes {
-            hash_map.insert(fake_hash.0, fake_hash.1);
+            table.insert(fake_hash.0);
             prog.increment();
         }
     }
 
     println!("\nFinished generating hash table in {:.2?}", now.elapsed());
 
-    hash_map
+    table
 }
 
-fn search(hash_map: FxHashMap<HashLastDigits, NumSpacesType>) {
+fn search_specific(h: &HashLastDigits) -> Option<NumSpacesType> {
+    let fakes_c = get_reversed_hashes(include_str!("confession_fake.txt"), NUM_HASHES);
+
+    while let Ok(fakes) = fakes_c.recv() {
+        for fake in fakes {
+            if fake.0 == *h {
+                return Some(fake.1);
+            }
+        }
+    }
+
+    return None;
+}
+
+fn search(table: TableType) {
     println!("Searching hash table for collisions...");
     let now = Instant::now();
 
-    let hash_map = Arc::new(hash_map);
+    let table = Arc::new(table);
 
     let handles = get_hashes_in_threads(
         include_str!("confession_real.txt"),
         NUM_HASHES,
         (0..HASH_SEARCH_WORKER_THREADS)
             .map(|worker_id| {
-                let hash_map = hash_map.clone();
+                let table = table.clone();
 
                 let mut prog = Progress::new((NUM_HASHES as usize) / HASH_SEARCH_WORKER_THREADS);
 
                 move |real_hashes: Vec<HashLastDigitsPair>| {
                     for real_hash in real_hashes {
-                        if let Some(matched) = hash_map.get(&real_hash.0) {
+                        if table.contains(&real_hash.0) {
                             println!(
-                                "\nCollision found with real {} fake {}\n",
-                                real_hash.1, matched
+                                "\nCollision found with real {}. Computing match for fake...\n",
+                                real_hash.1
+                            );
+
+                            println!(
+                                "\nCOLLISION OF LENGTH {} REAL {} FAKE {}\n",
+                                DESIRED_HEX_MATCHES,
+                                real_hash.1,
+                                search_specific(&real_hash.0).unwrap()
                             );
                         }
 
@@ -81,6 +104,6 @@ fn search(hash_map: FxHashMap<HashLastDigits, NumSpacesType>) {
 
 fn main() {
     println!();
-    let hm = gen_table();
-    search(hm);
+    let t = gen_table();
+    search(t);
 }
